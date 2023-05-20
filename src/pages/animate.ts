f534ee0d0a7218cc240aa000bc427e72672101b4
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2023-05-20 18:32:08 +0100

    feat: add animation

---
+++ src/pages/animate.ts
@@ -0,0 +1,121 @@
+import Splitting from 'splitting'
+import gsap from 'gsap'
+import { Observer } from 'gsap/Observer'
+gsap.registerPlugin(Observer)
+
+export const animate = () => {
+	Splitting()
+	const section: Element[] = gsap.utils.toArray('.section')
+	const figure: Element[] = gsap.utils.toArray('.figure')
+	const img: Element[] = gsap.utils.toArray('.img')
+
+	const wrap = gsap.utils.wrap(0, figure.length)
+	let animating: boolean
+	let currentIndex = 0
+
+	/**Reset */
+	gsap.set('.char', {
+		yPercent: 100,
+		willChange: 'transform',
+	})
+	gsap.set(img, {
+		xPercent: 100,
+	})
+
+	gsap.set('.section:nth-of-type(1) .char', {
+		yPercent: 0,
+	})
+	gsap.set(img[0], {
+		xPercent: 0,
+	})
+
+	function gotoSection(index: number, direction: number) {
+		animating = true
+		index = wrap(index)
+
+		let tl = gsap.timeline({
+			defaults: { duration: 1.5, ease: 'power4.inOut' },
+			onComplete: () => {
+				animating = false
+			},
+		})
+
+		const currentSection = section[currentIndex]
+		const text = currentSection.querySelectorAll('.char')
+		const nextSection = section[index]
+		const nextText = nextSection.querySelectorAll('.char')
+
+		const stagger = 0.011
+		const scaleY = 1.65
+
+		/**Animate here */
+		tl.fromTo(
+			img[currentIndex],
+			{ xPercent: 0, scaleY: 1 },
+			{
+				xPercent: -100 * direction,
+				scaleY,
+			}
+		)
+			.fromTo(
+				img[index],
+				{
+					xPercent: 100 * direction,
+					scaleY,
+				},
+				{ xPercent: 0, scaleY: 1 },
+				0
+			)
+			.fromTo(
+				text,
+				{ yPercent: 0 },
+				{
+					yPercent: -100 * direction,
+					stagger,
+				},
+				0
+			)
+			.fromTo(
+				nextText,
+				{
+					yPercent: 100 * direction,
+				},
+				{ yPercent: 0, stagger },
+				0
+			)
+
+		currentIndex = index
+	}
+
+	Observer.create({
+		type: 'wheel,touch,pointer',
+		preventDefault: true,
+		wheelSpeed: -1,
+		onUp: () => {
+			if (animating) return
+			gotoSection(currentIndex + 1, +1)
+		},
+		onDown: () => {
+			if (animating) return
+			gotoSection(currentIndex - 1, -1)
+		},
+		tolerance: 10,
+	})
+
+	document.addEventListener('keydown', logKey)
+
+	function logKey(e: { code: string }) {
+		if ((e.code === 'ArrowUp' || e.code === 'ArrowLeft') && !animating) {
+			gotoSection(currentIndex - 1, -1)
+		}
+		if (
+			(e.code === 'ArrowDown' ||
+				e.code === 'ArrowRight' ||
+				e.code === 'Space' ||
+				e.code === 'Enter') &&
+			!animating
+		) {
+			gotoSection(currentIndex + 1, 1)
+		}
+	}
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-import img from '@/assets/xplore.png'
+import { useEffectOnce } from '@studio-lumio/hooks'
 import { Fragment } from 'react'
+import { animate } from './animate'
+
+/**Assets */
+import img1 from '@/assets/img1.webp'
+import img2 from '@/assets/img2.webp'
+import img3 from '@/assets/img3.webp'
+import img4 from '@/assets/img4.webp'
 type IL = {
 	name: string
 	href: string
@@ -40,6 +47,17 @@ const Index = () => {
 		},
 	]
 
+	const children = [
+		{ title: `ABOUT:BLANK ‘23 WINTER COLLECTION`, img: img1 },
+		{ title: `OLA:BLANK ‘23 WINTER COLLECTION`, img: img2 },
+		{ title: `NULL:BLANK ‘23 WINTER COLLECTION`, img: img3 },
+		{ title: `LUMIO:BLANK ‘23 WINTER COLLECTION`, img: img4 },
+	]
+
+	useEffectOnce(() => {
+		animate()
+	})
+
 	return (
 		<Fragment>
 			<div className='grid'>
@@ -48,32 +66,40 @@ const Index = () => {
 				))}
 			</div>
 
-			<div className='index'>
-				<header>
-					<nav>
-						{hl.map((link) => (
-							<a href={link.href} key={link.name}>
-								{link.name}
-							</a>
-						))}
-					</nav>
-				</header>
+			<div className='parent'>
+				<div className='child'>
+					<header>
+						<nav>
+							{hl.map((link) => (
+								<a href={link.href} key={link.name}>
+									{link.name}
+								</a>
+							))}
+						</nav>
+					</header>
 
-				<h1>ABOUT:BLANK '23 WINTER COLLECTION</h1>
-				<figure>
-					<img src={img} alt='' width={928} height={682} />
-				</figure>
+					{children.map((child, index) => (
+						<div className='section' key={index}>
+							<h1 data-splitting='chars' className='text'>
+								{child.title}
+							</h1>
+							<figure className='figure'>
+								<img className='img' src={child.img} alt='' width={928} height={682} />
+							</figure>
+						</div>
+					))}
 
-				<footer>
-					<nav>
-						{fl.map((link) => (
-							<a href={link.href} key={link.name}>
-								{link.name}
-							</a>
-						))}
-					</nav>
-					<p>scroll down &#8595;</p>
-				</footer>
+					<footer>
+						<nav>
+							{fl.map((link) => (
+								<a href={link.href} key={link.name}>
+									{link.name}
+								</a>
+							))}
+						</nav>
+						<p>scroll down &#8595;</p>
+					</footer>
+				</div>
 			</div>
 		</Fragment>
 	)
